282d78453ef27c403a85cf5a580aa4ee43f4bf56
inigoSelwood <inigo.selwood@outlook.com>
2020-05-22 19:14:56 +0100

    Changed to use row-major order

--- matrix.hpp
+++ matrix.hpp
@@ -3,16 +3,16 @@
 // TODO: Implement a custom iterator
 class Matrix {
 
-private:
-
-    std::vector<std::vector<double>> values;
-
 public:
+
+    typedef std::array<unsigned int, 2> Size;
+    typedef std::array<unsigned int, 2> Index;
 
     static Matrix identity_matrix(const unsigned int &size);
 
     static double dot_product(const Matrix &one, const Matrix &two);
     static Matrix cross_product(const Matrix &one, const Matrix &two);
+    static Matrix power(const Matrix &matrix, const unsigned int &exponent);
 
     static Matrix operator*(const Matrix &matrix, const double &factor);
     static Matrix operator/(const Matrix &matrix, const double &factor);
@@ -41,170 +41,99 @@ public:
     unsigned int height() const;
     unsigned int width() const;
 
-    std::array<unsigned int, 2> size();
+    Size size();
     unsigned int volume() const;
+
+private:
+
+    std::vector<double> values;
+
+    static Index index(const unsigned int &offset);
+    static unsigned int offset(const Index &index);
 
 };
 
 // Prints an matrix to a stream
-std::ostream &operator<<(std::ostream &stream, const Matrix &matrix) {
-
-}
+std::ostream &operator<<(std::ostream &stream, const Matrix &matrix) {}
 
 // Prints an index to a stream
-std::ostream &operator<<(std::ostream &stream, const std::array<unsigned int, 2> &index) {
-    return stream << "(" << index[0] << ", " << index[1] << ")";
-}
+std::ostream &operator<<(std::ostream &stream, const std::array<unsigned int, 2> &index) {}
 
 // Returns the identity matrix of a given size
-Matrix Matrix::identity_matrix(const unsigned int &size) {
-    Matrix matrix(size, size);
-    for(unsigned int index = 0; index < size; index += 1)
-        matrix(index, index) = 1;
-    return matrix;
-}
+Matrix Matrix::identity_matrix(const unsigned int &size) {}
 
 // Returns the dot product of two matrices
-double Matrix::dot_product(const Matrix &one, const Matrix &two) {
-
-}
+double Matrix::dot_product(const Matrix &one, const Matrix &two) {}
 
 // Returns the cross product of two matrices
-Matrix Matrix::cross_product(const Matrix &one, const Matrix &two) {
+Matrix Matrix::cross_product(const Matrix &one, const Matrix &two) {}
 
-}
+// Returns the result of a matrix to a given power
+Matrix Matrix::power(const Matrix &matrix, const unsigned int &exponent);
 
 // Returns a matrix multiplied by a scalar factor
-Matrix Matrix::operator*(const Matrix &matrix, const double &factor) {
-    const auto size = matrix.size();
-    Matrix new_matrix = matrix;
-    for(unsigned int column = 0; column < size[0]; column += 1) {
-        for(unsigned int row = 0; row < size[1]; row += 1)
-            new_matrix(column, row) *= factor;
-    }
-    return new_matrix;
-}
+Matrix Matrix::operator*(const Matrix &matrix, const double &factor) {}
 
 // Returns a matrix divided by a scalar factor
-Matrix Matrix::operator/(const Matrix &matrix, const double &factor) {
-    return matrix * (1 / factor);
-}
+Matrix Matrix::operator/(const Matrix &matrix, const double &factor) {}
 
 // Returns the sum of two matrices
-Matrix Matrix::operator+(const Matrix &one, const Matrix &two) {
-    if(one.size() != two.size()) {
-        std::cerr << "Can't add two matrices of different sizes" << std::endl;
-        throw -1;
-    }
-
-    const auto size = one.size();
-    Matrix new_matrix = one;
-    for(unsigned int column = 0; column < size[0]; column += 1) {
-        for(unsigned int row = 0; row < size[1]; row += 1)
-            new_matrix(column, row) += two(column, row);
-    }
-    return new_matrix;
-}
+Matrix Matrix::operator+(const Matrix &one, const Matrix &two) {}
 
 // Returns true if the two matrices aren't equal
-bool Matrix::operator!=(const Matrix &one, const Matrix &two) {
-    return (one == two) == false;
-}
+bool Matrix::operator!=(const Matrix &one, const Matrix &two) {}
 
 // Returns true if the matrices are equal
-bool Matrix::operator==(const Matrix &one, const Matrix &two) {
-    if(one.size() != two.size())
-        return false;
-
-    const auto epsilon = std::numeric_limits<double>::epsilon();
-    for(unsigned int column = 0; column < size[0]; column += 1) {
-        for(unsigned int row = 0; row < size[1]; row += 1) {
-            if(std::fabs(one(column, row) - two(column, row)) >= epsilon)
-                return false;
-        }
-    }
-
-    return true;
-}
+bool Matrix::operator==(const Matrix &one, const Matrix &two) {}
 
 // Returns the value at a given column and row index
 double &Matrix::operator()(const unsigned int &column,
-        const unsigned int &row) {
-
-    if(column >= values.size() || row >= values[column].size())
-        return 0;
-
-
-}
+        const unsigned int &row) {}
 
 // Multiplies the matrix by a scalar factor
-Matrix &Matrix::operator*=(const double &factor) {
-
-}
+Matrix &Matrix::operator*=(const double &factor) {}
 
 // Divides the matrix by a scalar factor
-Matrix &Matrix::operator/=(const double &factor) {
-
-}
+Matrix &Matrix::operator/=(const double &factor) {}
 
 // Adds a matrix to this matrix
-Matrix &Matrix::operator+=(const Matrix &matrix) {
+Matrix &Matrix::operator+=(const Matrix &matrix) {}
 
-}
+Matrix::Matrix() {}
 
-Matrix::Matrix() {
+Matrix::Matrix(const std::vector<std::vector<double>> &values) {}
 
-}
-
-Matrix::Matrix(const std::vector<std::vector<double>> &values) {
-
-}
-
-Matrix::Matrix(const unsigned int &width, const unsigned int &height) {
-
-}
+Matrix::Matrix(const unsigned int &width, const unsigned int &height) {}
 
 // Resizes the matrix, copying any overlapping data into the resized matrix
-void Matrix::resize(const unsigned int &width, const unsigned int &height) {
-
-}
+void Matrix::resize(const unsigned int &width, const unsigned int &height) {}
 
 // Clear the values in the matrix, leaving its size intact
-void Matrix::clear() {
-
-}
+void Matrix::clear() {}
 
 // Returns the derivative of the matrix
-double Matrix::derivative() const {
-
-}
+double Matrix::derivative() const {}
 
 // Returns the inverse of the matrix
-Matrix Matrix::inverse() const {
-
-}
+Matrix Matrix::inverse() const {}
 
 // Returns the transpose of the matrix
-Matrix Matrix::transpose() const {
-
-}
+Matrix Matrix::transpose() const {}
 
 // Returns the height of the matrix
-unsigned int Matrix::height() const {
-
-}
+unsigned int Matrix::height() const {}
 
 // Returns the width of the matrix
-unsigned int Matrix::width() const {
-
-}
+unsigned int Matrix::width() const {}
 
 // Returns the size of the matrix, in the format {width, height}
-std::array<unsigned int, 2> Matrix::size() {
-
-}
+std::array<unsigned int, 2> Matrix::size() {}
 
 // Returns the volume of the matrix (width * height)
-unsigned int Matrix::volume() const {
+unsigned int Matrix::volume() const {}
 
-}
+// Returns the index corresponding to a row-major order offset
+Index Matrix::index(const unsigned int &offset) {}
+
+// Returns the row-major order offset of a given index
+unsigned int Matrix::offset(const Index &index) {}
