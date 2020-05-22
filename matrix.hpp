1c95b294882cb44fa3aaf0399d6fe92a04a46dd7
inigoSelwood <inigo.selwood@outlook.com>
2020-05-22 15:48:18 +0100

    Added documentation

--- matrix.hpp
+++ matrix.hpp
@@ -8,7 +8,7 @@ private:
 
 public:
 
-    static Matrix identity_matrix(const unsigned int &dimension);
+    static Matrix identity_matrix(const unsigned int &size);
 
     static double dot_product(const Matrix &one, const Matrix &two);
     static Matrix cross_product(const Matrix &one, const Matrix &two);
@@ -45,55 +45,68 @@ public:
 
 };
 
+// Prints the matrix to a stream
 std::ostream &Matrix::operator<<(std::ostream &stream, const Matrix &matrix) {
-    
-}
-
-Matrix Matrix::identity_matrix(const unsigned int &dimension) {
 
 }
 
+// Returns the identity matrix of a given size
+Matrix Matrix::identity_matrix(const unsigned int &size) {
+
+}
+
+// Returns the dot product of two matrices
 double Matrix::dot_product(const Matrix &one, const Matrix &two) {
 
 }
 
+// Returns the cross product of two matrices
 Matrix Matrix::cross_product(const Matrix &one, const Matrix &two) {
 
 }
 
+// Returns a matrix multiplied by a scalar factor
 Matrix Matrix::operator*(const Matrix &one, const double &factor) {
 
 }
 
+// Returns a matrix divided by a scalar factor
 Matrix Matrix::operator/(const Matrix &one, const double &factor) {
 
 }
 
+// Returns the sum of two matrices
 Matrix Matrix::operator+(const Matrix &one, const Matrix &two) {
 
 }
 
+// Returns true if the two matrices aren't equal
 bool Matrix::operator!=(const Matrix &one, const Matrix &two) {
 
 }
 
+// Returns true if the matrices are equal
 bool Matrix::operator==(const Matrix &one, const Matrix &two) {
 
 }
 
+// Returns the value at a given column and row index
 double &Matrix::operator()(const unsigned int &column,
         const unsigned int &row) {
 
 }
 
+// Multiplies the matrix by a scalar factor
 Matrix &Matrix::operator*=(const double &factor) {
 
 }
 
+// Divides the matrix by a scalar factor
 Matrix &Matrix::operator/=(const double &factor) {
 
 }
 
+// Adds a matrix to this matrix
 Matrix &Matrix::operator+=(const Matrix &matrix) {
 
 }
@@ -110,38 +123,47 @@ Matrix::Matrix(const unsigned int &width, const unsigned int &height) {
 
 }
 
+// Resizes the matrix, copying any overlapping data into the resized matrix
 void Matrix::resize(const unsigned int &width, const unsigned int &height) {
 
 }
 
+// Clear the values in the matrix, leaving its size intact
 void Matrix::clear() {
 
 }
 
+// Returns the derivative of the matrix
 double Matrix::derivative() const {
 
 }
 
+// Returns the inverse of the matrix
 Matrix Matrix::inverse() const {
 
 }
 
+// Returns the transpose of the matrix
 Matrix Matrix::transpose() const {
 
 }
 
+// Returns the height of the matrix
 unsigned int Matrix::height() const {
 
 }
 
+// Returns the width of the matrix
 unsigned int Matrix::width() const {
 
 }
 
+// Returns the size of the matrix, in the format {width, height}
 std::array<unsigned int, 2> Matrix::size() {
 
 }
 
+// Returns the volume of the matrix (width * height)
 unsigned int Matrix::volume() const {
 
 }
