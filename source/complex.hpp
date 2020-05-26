36c8107eea778df183fd303fa0ccc27a4d5ec658
inigoSelwood <inigo.selwood@outlook.com>
2020-05-26 16:47:38 +0100

    Added documentation

--- source/complex.hpp
+++ source/complex.hpp
@@ -22,6 +22,7 @@ public:
     Complex &operator+=(const double &value);
 
     Complex();
+    Complex(const double &real_part);
     Complex(const double &real_part, const double &imaginary_part);
 
     double magnitude() const;
@@ -31,6 +32,7 @@ public:
 
 // ******************************************************* Stream print operator
 
+// Prints a complex number to the stream in the format: "a +/- bi"
 std::ostream &operator<<(std::ostream &stream, const Complex &complex) {
     return stream << complex.real_part <<
             ((complex.imaginary_part < 0) ? " - " : " + ") <<
@@ -73,17 +75,20 @@ Complex operator+(const Complex &one, const double &value) {
 
 // ****************************************************** Unary member operators
 
+// (a + bi) - (c + di) = (a - c) + (b - d)i
 Complex &Complex::operator-=(const Complex &value) {
     real_part -= value.real_part;
     imaginary_part -= value.imaginary_part;
     return *this;
 }
 
+// (a + bi) - c = (a - c) + bi
 Complex &Complex::operator-=(const double &value) {
     real_part -= value;
     return *this;
 }
 
+// (a + bi) * (c + di) = ac - bd + (ad + bc)i
 Complex &Complex::operator*=(const Complex &value) {
     Complex result;
     result.real_part = (real_part * value.real_part) -
@@ -94,12 +99,14 @@ Complex &Complex::operator*=(const Complex &value) {
     return *this;
 }
 
+// (a + bi) * c = ac + (bc)i
 Complex &Complex::operator*=(const double &factor) {
     real_part *= factor;
     imaginary_part *= factor;
     return *this;
 }
 
+// (a + bi) / (c + di) = ((ac + bd) / (c^2 + d^2)) + ((bc - ad) / (c^2 + d^2))i
 Complex &Complex::operator/=(const Complex &value) {
     const double denominator = std::pow(value.real_part, 2) +
             std::pow(value.imaginary_part, 2);
@@ -115,18 +122,21 @@ Complex &Complex::operator/=(const Complex &value) {
     return *this;
 }
 
+// (a + bi) / c = (a / c) + (b / c)i
 Complex &Complex::operator/=(const double &factor) {
     real_part /= factor;
     imaginary_part /= factor;
     return *this;
 }
 
+// (a + bi) + (c + di) = (a + c) + (b + d)i
 Complex &Complex::operator+=(const Complex &value) {
     real_part += value.real_part;
     imaginary_part += value.imaginary_part;
     return *this;
 }
 
+// (a + bi) + c = (a + c) + bi
 Complex &Complex::operator+=(const double &value) {
     real_part += value;
     return *this;
@@ -135,6 +145,11 @@ Complex &Complex::operator+=(const double &value) {
 Complex::Complex() {
     real_part = 0;
     imaginary_part = 0;
+}
+
+Complex::Complex(const double &real_part) {
+    this->real_part = real_part;
+    this->complex_part = 0;
 }
 
 Complex::Complex(const double &real_part, const double &imaginary_part) {
