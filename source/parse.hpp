4a5873f1cd2a5e21439e1f096cf9c33a57af5100
inigoSelwood <inigo.selwood@outlook.com>
2020-06-04 18:19:46 +0100

    Added position get/set functions

--- source/parse.hpp
+++ source/parse.hpp
@@ -19,25 +19,37 @@ or skipped, relative to the position of the index within the text. There are
 also functions for conditional skipping, which will return a character/string
 if a matching one is provided.
 
-The source file is split into [] parts:
+The position sub-struct is used to move between set points in the buffer. Say
+a given parse function branches in two directions depending on information which
+is only available at a later time, a position struct can be extracted at the
+start, and then potentially rewound to at a later time -- without having to use
+the more computationally expensive decrement function.
 
-    (1) Increment/decrement operators
+The source file is split into 9 parts:
+
+    (1) Definition
+
+    (2) Print operator
+        This makes printing the position of an unexpected character/string
+        easier from the perspective of parse functions
+
+    (3) Increment/decrement operators
         These functions allow the index position to be moved around within the
         text. This also updates internal members like the line and column
         number -- although the position information functions which these
         supported have been removed in the current context.
 
-    (2) Constructors
+    (4) Constructors
 
-    (3) Get functions
+    (5) Get functions
         These functions allow characters/strings to be accessed at the current
         index, without skipping past them. It can be more computationally
         expensive to use these functions instead of skip functions, so use them
         sparingly? I suppose?
 
-    (4) End reached test
+    (6) End reached test
 
-    (5) Skip functions
+    (7) Skip functions
         I'm quite proud of myself for coming up with this one, they're
         especially helpful with parsing markup languages like JSON and YAML.
         Basically, you provide a character/string, and the Buffer class will
@@ -45,9 +57,15 @@ The source file is split into [] parts:
         This way, the function will return 'false', if the character/text isn't
         present.
 
-    (6) Parse helpers
+    (8) Parse helpers
         These functions make it easier to extract fields from the text, such as
         integers, values in scientific notation, metric values, etc.
+
+    (9) Position helpers
+        These functions allow you to change the index's position within the
+        buffer, or get a position struct with information about this current
+        position. This position class has an std::ostream print operator, which
+        neatly serializes the line and column data
 
 */
 
@@ -108,7 +126,16 @@ public:
     void skip_whitespace(const int flags);
     bool skip_string(const std::string &text);
 
+    Buffer::Position Buffer::get_position() const;
+    void Buffer::set_position(const Position &position);
+
 };
+
+// ************************************************************** Print operator
+
+std::ostream &operator(std::ostream &stream, const Buffer::Position &position) {
+    return stream << "line " << position.line << " column " << position.column;
+}
 
 // *********************************************** Increment/decrement operators
 
@@ -144,7 +171,7 @@ void Buffer::decrement(const unsigned int steps = 1) {
 // **************************************************************** Constructors
 
 Buffer::Buffer(const std::string &text) {
-    if(_text.empty())
+    if(text.empty())
         return;
     _text = text;
 
@@ -291,7 +318,7 @@ static const std::unordered_set<char> metric_prefixes = {'f', 'p', 'n', 'u',
 // True if the character is a metric prefix symbol (micro 'u', nano 'n', etc.)
 // NOTE: This isn't 100% reliable in the case of the "Meg" prefix
 static bool is_metric_symbol(const char &character) {
-    return metric_prefixes.find(character) == metric_prefixes.end();
+    return metric_prefixes.find(character) != metric_prefixes.end();
 }
 
 // True if the character is 0-9
@@ -330,6 +357,7 @@ static inline int parse_metric_symbol(Buffer &buffer) {
             return 12;
     }
 
+    std::cerr << "Couldn't recognize metric symbol" << std::endl;
     throw -1;
 }
 
@@ -339,8 +367,10 @@ int integer(Buffer &buffer) {
     while(is_integer(buffer.get_current()))
         value += buffer.skip_current();
 
-    if(value.empty())
+    if(value.empty()) {
+        std::cerr << "Expected integer" << std::endl;
         throw -1;
+    }
 
     return std::stoi(value);
 }
@@ -351,8 +381,10 @@ unsigned int natural_number(Buffer &buffer) {
     while(is_natural_number(buffer.get_current()))
         value += buffer.skip_current();
 
-    if(value.empty())
+    if(value.empty()) {
+        std::cerr << "Expected natural number" << std::endl;
         throw -1;
+    }
 
     return std::stoi(value);
 }
@@ -363,8 +395,10 @@ double number(Buffer &buffer) {
     while(is_number(buffer.get_current()))
         value += buffer.skip_current();
 
-    if(value.empty())
+    if(value.empty()) {
+        std::cerr << "Expected number" << std::endl;
         throw -1;
+    }
 
     return std::stof(value);
 }
@@ -387,6 +421,28 @@ double metric_value(Buffer &buffer) {
         string_value += buffer.skip_current();
 
     return std::stof(string_value) * std::pow(10, factor);
+}
+
+// ************************************************************ Position helpers
+
+// Gets the buffer's position
+// Position data includes the column, line, and current index
+Buffer::Position Buffer::get_position() const {
+    return Position(_index, _line + 1, _column + 1);
+}
+
+// Sets the buffer's position
+// If the line/column data doesn't match the index, an error is thrown
+void Buffer::set_position(const Position &position) {
+    if(_index != (_line_start_indices[_line] + _column)) {
+        std::cerr << "Couldn't set position within parse buffer, since the "
+                "index data didn't match internal data" << std::endl;
+        throw -1;
+    }
+
+    _index = position.index;
+    _line = position.line;
+    _column = position.column;
 }
 
 }; // Namespace Parse
