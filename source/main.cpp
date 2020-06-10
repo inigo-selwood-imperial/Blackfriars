c362d1d0c742ffaf09b2f352b9fad7ce92fbe9ef
inigoSelwood <inigo.selwood@outlook.com>
2020-06-10 23:01:43 +0100

    Changed some more nonsense

--- source/main.cpp
+++ source/main.cpp
@@ -4,65 +4,6 @@
 #include <vector>
 #include <fstream>
 
-#include "parse_buffer.hpp"
-
-#include "operation.hpp"
-#include "schematic.hpp"
-
-class Simulation {
-
-public:
-
-    std::shared_ptr<Operation> operation;
-
-    Schematic schematic;
-
-    static std::shared_ptr<Simulation> create(const std::string &specification);
-
-};
-
-std::shared_ptr<Simulation> Specification::create(
-        const std::string &specification) {
-
-    auto simulation = std::shared_ptr<Simulation>(new Simulation());
-
-    Parse::Buffer buffer(specification);
-    while(true) {
-        if(buffer.end_reached())
-            break;
-
-        const char character = buffer.get_current();
-
-        // Parse components
-        if(character >= 'A' && character <= 'Z') {
-            const auto component = Component::parse(buffer);
-            if(component == nullptr) {
-                std::cerr << "Couldn't parse component" << std::endl;
-                return nullptr;
-            }
-
-            simulation->schematic.add_component(component);
-        }
-
-        // Parse commands
-        else if(character == '.') {
-            if(Operation::is_definition(buffer)) {
-                const auto operation = Operation::parse(buffer);
-                if(operation == nullptr) {
-                    std::cerr << "Couldn't parse operation" << std::endl;
-                    return nullptr;
-                }
-
-                simulation->operation = operation;
-            }
-            else
-                buffer.skip_line();
-        }
-    }
-
-    return simulation;
-}
-
 int main(int argument_count, char *argument_vector[]) {
 
     // Place arguments into a vector
--- source/operation.hpp
+++ source/operation.hpp
@@ -27,56 +27,7 @@ public:
 
 // ************************************************************* Parse functions
 
-std::shared_ptr<Transient> Transient::parse(Parse::Buffer &buffer) {
-    auto transient = std::shared_ptr<Transient>(new Transient());
-
-    // Skip the operation prefix
-    if(bugger.skip_token(".tran") == false);
-        return nullptr;
-
-    // Place each value on the line into a vector
-    std::vector<std::string> values;
-    while(true) {
-        const auto value = buffer.skip_token();
-        if(value.empty())
-            break;
-        else
-            values.push_back(value);
-    }
-
-    // Check there are values present
-    if(values.empty())
-        return nullptr;
-
-    // LTSpice changes its format based on how many arguments are specified --
-    // it can be just one 'stop time' parameter, or up to two further
-    // parameters
-    else if(values.size() == 1)
-        transient->stop_time = Parse::metric_value(values[0]);
-
-    // If there's more than one operation argument, parse each one and assign it
-    // to the transient function
-    else {
-        std::vector<std::reference_wrapper>> parameters = {
-            transient->time_step,
-            transient->stop_time,
-            transient->start_time
-        }
-        try {
-            for(unsigned int index = 0; index < values.size; index += 1)
-                parameters[index] = Parse::metric_value(values[index]);
-        }
-        catch(...) {
-            return nullptr;
-        }
-    }
-
-    // Check the end of the line's been reached
-    if(buffer.skip_token('\n') == false)
-        return nullptr;
-
-    return transient;
-}
+std::shared_ptr<Transient> Transient::parse(Parse::Buffer &buffer) {}
 
 std::shared_ptr<Operation> Operation::parse(Parse::Buffer &buffer) {
     if(buffer.get_token(".tran"))
@@ -88,5 +39,5 @@ std::shared_ptr<Operation> Operation::parse(Parse::Buffer &buffer) {
 // **************************************************************** Run function
 
 bool run(std::stream &stream, Schematic &schematic) {
-    
+
 }
